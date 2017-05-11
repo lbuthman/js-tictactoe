19fe9b73770ca927162786892033876fe8c2b49f
lbuthman <lbuthman@gmail.com>
2017-05-11 09:31:27 -0500

    Add reset game and start over functions with transition

--- logic.js
+++ logic.js
@@ -1,24 +1,19 @@
+/*Constants******************************/
+
 var X = "https://cdn4.iconfinder.com/data/icons/heros/100/Super_Hero_1-512.png";
 var O = "https://maxcdn.icons8.com/Color/PNG/512/Cinema/joker_suicide_squad-512.png";
-var BOARD = {
-    1: "empty",
-    2: "empty",
-    3: "empty",
-    4: "empty",
-    5: "empty",
-    6: "empty",
-    7: "empty",
-    8: "empty",
-    9: "empty"
-};
-
-//One of two: one-player, two-players
-var gameMode = "";
 var showSpeed = 500;
 var hideSpeed = 1000;
 
+/*Variables*******************************/
+
+//One of two: one-player, two-players
+var gameMode = "";
+
 //One of two: Batman - X, Jokeer - O
 var playerToken = "";
+
+/*Button Clicks***************************/
 
 /*Start game with Player selection, hiding other elements*/
 $(document).ready(function() {
@@ -63,8 +58,44 @@ $(".token").click(function() {
 });
 
 /*Handle reset buttons*/
+$(".reset").click(function() {
+    var type = this.id;
+    switch (type) {
+        case "reset-game":
+            gameInit();
+            break;
+        case "reset-all":
+            startOver();
+            break;
+        default:
+    }
 
+});
 
+/*Functions**************************/
+
+function startOver() {
+
+    gameMode = "";
+    playerToken = "";
+    $("#board").hide(hideSpeed);
+    $("#choose-players").show(showSpeed);
+    gameInit();
+}
+
+function gameInit() {
+    var BOARD = {
+        1: "empty",
+        2: "empty",
+        3: "empty",
+        4: "empty",
+        5: "empty",
+        6: "empty",
+        7: "empty",
+        8: "empty",
+        9: "empty"
+    };
+}
 
 $("#top-left").click(function() {
     $("#top-left").html("<img class='img-thumbnail' src='" + X + "' alt='batman' height='90' width='100'>");
@@ -77,7 +108,3 @@ $("#top-center").click(function() {
 
 $("#top-right").click(function() {
     $("#top-right").html("<img class='img-thumbnail' src='" + X + "' alt='batman' height='90' width='100'>")});
-
-function renderImage() {
-
-}
